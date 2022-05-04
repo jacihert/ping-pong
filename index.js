deebdb61baeb78e23b3d4d0673e1eb310dd3f074
jacintha <jacintha.h@gmail.com>
2022-05-04 20:38:10 +0200

    Version 5

--- index.js
+++ index.js
@@ -1,35 +1,42 @@
 let interval = null;
+
 let boardTop = 0;
 let boardBottom = window.innerHeight;
 let boardLeft = 20;
 let boardRight = window.innerWidth-20;
-let ballPositionLeft = window.innerWidth / 2;
-let ballPositionTop = window.innerHeight - 20;
+
 const initialBallPositionLeft = window.innerWidth / 2;
-const initialBballPositionTop = window.innerHeight - 20;
-let Paddle1PositionLeft = 20;
-let Paddle1PositionTop = window.innerHeight / 2 - 20;
-let Paddle2PositionLeft = window.innerWidth-20-20;
-let Paddle2PositionTop = window.innerHeight / 2;
+const initialBallPositionTop = window.innerHeight - 20;
+let ballPositionLeft = initialBallPositionLeft;
+let ballPositionTop = initialBallPositionTop;
 let dx = 1;
 let dy = 1;
+
+const Paddle1PositionLeft = '20 px';
+let Paddle1PositionTop = window.innerHeight / 2 - 40;
+
+const Paddle2PositionLeft = window.innerWidth-20-20 + 'px'
+let initialPaddle2PositionTop = window.innerHeight / 2- 40;
+let Paddle2PositionTop = initialPaddle2PositionTop;
+
 
 //  initalise paddles & ball positions
 const initialiseAll = () => {
         //  initalise paddle positions
+    paddle1.style.left = Paddle1PositionLeft;
     paddle1.style.top = Paddle1PositionTop + "px";
-    paddle1.style.left = Paddle1PositionLeft + "px";
-
-    paddle2.style.top = Paddle2PositionTop + "px";
-    paddle2.style.left = Paddle2PositionLeft + "px";
-
+    
+    Paddle2PositionTop = initialPaddle2PositionTop;
+    paddle2.style.left = Paddle2PositionLeft;
+    paddle2.style.top = initialPaddle2PositionTop + "px";
+    
         //   initalise ball  position
-    ball.style.top = initialBballPositionTop + "px";
+    ball.style.top = initialBallPositionTop + "px";
     ball.style.left = initialBallPositionLeft + "px";
+    ballPositionLeft = initialBallPositionLeft 
+    ballPositionTop = initialBallPositionTop
     dx = 1;
     dy = 1;
-    ballPositionLeft = initialBallPositionLeft 
-    ballPositionTop = initialBballPositionTop
 
         //  set score
     document.getElementById("score1").innerHTML = 0;
@@ -50,40 +57,48 @@ const bouncingBall = () => {
   ball.style.top = ballPositionTop + "px";
 };
 
+// ------------------ START ------------------------------------------------------------------
 initialiseAll();
+// ------------------ PLAY GAME using keys-----------------------------------------------------
 document.addEventListener("keydown", (event) => {
-    console.log(event.key);
+    
     // Play
     if (!interval && (event.key === "P" || event.key === "p")) {
-    interval = setInterval(bouncingBall, 1/10);
+        interval = setInterval(bouncingBall, 1/10);
     }
     // Stop / pause
     if (event.key === "S" || event.key === "s") {
-    clearInterval(interval);
-    interval = null;
+        clearInterval(interval);
+        interval = null;
     }
     // Restart
     if (!interval && (event.key === "R" || event.key === "r")) {
-    clearInterval(interval);
-    interval = null;
-    initialiseAll();
-    interval = setInterval(bouncingBall, 1/10);
+        clearInterval(interval);
+        interval = null;
+        initialiseAll();
+        interval = setInterval(bouncingBall, 1/10);
     }
-                // if(event.key==="ArrowLeft"){   //move paddle up
-// if(event.key==="ArrowUp"){
-//     //interval = setInterval(moveLeft, 1);
-// }
-                // if(event.key==="ArrowRight") {     //move paddle down
-// if(event.key==="ArrowDown"){
-//     //interval = setInterval(moveRight, 1);
-// }
+    // move right paddle down
+    if(interval && event.key==="ArrowUp"){
+        if (Paddle2PositionTop >= boardTop){
+            Paddle2PositionTop -= 15
+            paddle2.style.top = Paddle2PositionTop  + "px";
+        } else {
+            paddle2.style.top = 0  + "px";
+            }
+        
+    }
+    // //  move right paddle down
+    if(interval && event.key==="ArrowDown"){
+        if (Paddle2PositionTop+80 < boardBottom ){
+            Paddle2PositionTop += 15
+            paddle2.style.top = Paddle2PositionTop  + "px";
+        } else {
+            paddle2.style.bottom = boardBottom  + "px";
+            }
+    }
 });
 
 //----------------------------------------------------------------------------------------------
-// move the ball from top left to bottom right and so on 
-
-// when ball touches paddle then to move back. else to stop  and score to be calculated 
-
-//player 1 paddle to move automatically to the point where the ball will go to  X(0) position
-// based on the players arrow keys mmove paddle 2 to move back n forth
+// To DO : when ball touches paddle then to move back. else to stop and score to be calculated 
 //----------------------------------------------------------------------------------------------
